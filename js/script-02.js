5cdfc03be92325ed86f34268bb94a2a6d77606e8
Andrii Lypovetskyi <li_andr@ukr.net>
2021-05-08 15:28:49 +0300

    Задача1 и Задача2

---
+++ js/script-02.js
@@ -0,0 +1,8 @@
+let productName = "Droid";
+let pricePerItem = 2000;
+
+// Change code below this line
+productName = "Repair droid";
+pricePerItem = pricePerItem + 1500;
+console.log(productName);
+console.log(pricePerItem);
