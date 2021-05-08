be5b1aa3f58599e43cc454a9ac97ca05e9d2dbf4
Andrii Lypovetskyi <li_andr@ukr.net>
2021-05-08 20:23:00 +0300

    Задачи9-11

---
+++ js/script-12.js
@@ -0,0 +1,12 @@
+function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+  // Change code below this line
+
+const message = `You ordered droids worth ${orderedQuantity*pricePerDroid+deliveryFee} redits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+  // Change code above this line
+    return message;
+}
+
+makeOrderMessage(2, 100, 50);
+
+  console.log(message); 
