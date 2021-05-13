6543dce96c7a48aca222ff8d095bdcb654a660d1
Andrii Lypovetskyi <li_andr@ukr.net>
2021-05-13 11:44:46 +0300

    Задача30-33

---
+++ js/script-30.js
@@ -0,0 +1,15 @@
+// Задание
+// Функция getNameLength(name) принимает имя (параметр name) и возвращает строку, в которой указана его длина. Дополни шаблонную строку в переменной message длиной строки из параметра name.
+
+// Тесты
+// Объявлена функция getNameLength(name)
+// Вызов функции getNameLength("Poly") возвращает "Name Poly is 4 characters long"
+// Вызов функции getNameLength("Harambe") возвращает "Name Harambe is 6 characters long"
+// Вызов функции getNameLength("Billy") возвращает "Name Billy is 5 characters long"
+// Вызов функции getNameLength("Joe") возвращает "Name Joe is 3 characters long"
+
+function getNameLength(name) {
+  const message = `Name ${name} is ${name.length} characters long`; // Change this line
+
+  return message;
+}
---
+++ js/script-31.js
@@ -0,0 +1,25 @@
+// Задание
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим элементам или свойствам строки в переменной course.
+
+// courseTopicLength - длина строки.
+// firstElement - первый символ строки.
+// lastElement - последний символ строки.
+// Тесты
+// Объявлена переменная courseTopic
+// Значение переменной courseTopic это строка "JavaScript essentials"
+// Объявлена переменная courseTopicLength
+// Значение переменной courseTopicLength это число 24
+// Объявлена переменная firstElement
+// Значение переменной firstElement это строка "J"
+// Объявлена переменная lastElement
+// Значение переменной lastElement это строка "s"
+
+
+const courseTopic = "JavaScript essentials";
+// Change code below this line
+
+const courseTopicLength = courseTopic.length;
+const firstElement = courseTopic[0];
+const lastElement = courseTopic[courseTopicLength - 1];
+
+// Change code above this line
